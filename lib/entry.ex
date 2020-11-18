b193675727f0912855a74277eca9262bd28b1f8a
Brandon Keown <me@brandonkeown.com>
2020-11-18 08:16:15 -0600

    Shift to being able to produce a binary

--- lib/entry.ex
+++ lib/entry.ex
@@ -1,15 +1,18 @@
 defmodule EntryPoint do
-  def run(mod) do
-    then = Time.utc_now()
-    input = IO.read(:stdio, :all)
-      |> String.trim()
+  def main(args) do
+    which = hd(args)
+    input = File.read("inputs/#{which |> String.downcase}.txt") |> Pair.snd |> String.trim
+    run(String.to_existing_atom("Elixir.#{hd(args)}"), input)
+  end
+  def run(mod, input) do
+    {run_time, _} = :timer.tc(fn ->
     solution1 = mod.part1(input)
     solution2 = mod.part2(input)
     IO.puts("Running #{mod}")
     IO.puts("Solution 1: #{solution1}")
     IO.puts("Solution 2: #{solution2}")
-    now = Time.utc_now()
-    delta = Time.diff(now, then, :millisecond)
-    IO.puts("Time: #{delta}ms")
+    end)
+
+    IO.puts("Time: #{run_time/1000}ms")
   end
 end
